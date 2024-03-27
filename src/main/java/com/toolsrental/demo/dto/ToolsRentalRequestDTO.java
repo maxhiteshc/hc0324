bec6cdb1becf4bb9cac8ad88c0bf760e55f2b630
hiteshashokchhabria <hitesh.chhabria@wolfe.com>
2024-03-28 01:05:07 +0530

    Initial changes

--- src/main/java/com/toolsrental/demo/dto/ToolsRentalRequestDTO.java
+++ src/main/java/com/toolsrental/demo/dto/ToolsRentalRequestDTO.java
@@ -1,4 +1,22 @@
 package com.toolsrental.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ToolsRentalRequestDTO {
+
+    private String toolCode;
+
+    private int rentalDaysCount;
+
+    private int discountPercent;
+
+    private String checkoutDate;
+
 }
