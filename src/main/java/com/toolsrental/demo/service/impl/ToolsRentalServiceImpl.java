f7c63798e56640871f71a2fc7d2433abea72f5c9
hiteshashokchhabria <hitesh.chhabria@wolfe.com>
2024-03-28 01:17:44 +0530

    Initial changes

--- src/main/java/com/toolsrental/demo/service/impl/ToolsRentalServiceImpl.java
+++ src/main/java/com/toolsrental/demo/service/impl/ToolsRentalServiceImpl.java
@@ -7,7 +7,8 @@ import com.toolsrental.demo.service.ToolsRentalService;
 public class ToolsRentalServiceImpl implements ToolsRentalService {
     public ToolsRentalResponseDTO checkout(ToolsRentalRequestDTO toolsRentalRequestDTO) {
         ToolsRentalResponseDTO toolsRentalResponseDTO = new ToolsRentalResponseDTO();
-        //// TODO: 28/03/24 Generate and calculate Response values using request 
+        // TODO: 28/03/24 Generate and calculate Response values using request
+        // Check for holidays and weekends applicable
         return toolsRentalResponseDTO;
     }
 }
