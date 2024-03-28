e611d11403a95325616dbb4115d53c916eb03018
hiteshashokchhabria <hitesh.chhabria@wolfe.com>
2024-03-28 19:04:20 +0530

    Initial changes

--- src/main/java/com/toolsrental/demo/service/impl/ToolsRentalServiceImpl.java
+++ src/main/java/com/toolsrental/demo/service/impl/ToolsRentalServiceImpl.java
@@ -5,13 +5,15 @@ import com.toolsrental.demo.constants.Tools;
 import com.toolsrental.demo.dto.ToolsRentalRequestDTO;
 import com.toolsrental.demo.dto.ToolsRentalResponseDTO;
 import com.toolsrental.demo.service.ToolsRentalService;
+import jdk.jshell.spi.ExecutionControl;
+import org.springframework.validation.method.MethodValidationException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 public class ToolsRentalServiceImpl implements ToolsRentalService {
-    public ToolsRentalResponseDTO checkout(ToolsRentalRequestDTO toolsRentalRequestDTO) {
+    public ToolsRentalResponseDTO checkout(ToolsRentalRequestDTO toolsRentalRequestDTO) throws IllegalArgumentException {
         ToolsRentalResponseDTO toolsRentalResponseDTO = new ToolsRentalResponseDTO();
         toolsRentalResponseDTO.setToolCode(toolsRentalRequestDTO.getToolCode());
 
@@ -19,6 +21,9 @@ public class ToolsRentalServiceImpl implements ToolsRentalService {
         toolsRentalResponseDTO.setToolType(tools.getToolType());
         toolsRentalResponseDTO.setToolBrand(tools.getBrand());
 
+        if(toolsRentalRequestDTO.getRentalDaysCount() < 1 ) {
+            throw new IllegalArgumentException("Rental day count is not 1 or greater");
+        }
         toolsRentalResponseDTO.setRentalDaysCount(toolsRentalRequestDTO.getRentalDaysCount());
         toolsRentalResponseDTO.setCheckoutDate(toolsRentalRequestDTO.getCheckoutDate());
 
@@ -40,6 +45,11 @@ public class ToolsRentalServiceImpl implements ToolsRentalService {
         toolsRentalResponseDTO.setPreDiscountCharge(preDiscountCharge);
 
         int discountPercent = toolsRentalRequestDTO.getDiscountPercent();
+
+
+        if(discountPercent < 0 || discountPercent > 100 ) {
+            throw new IllegalArgumentException("Discount percent is not in the range 0-100");
+        }
         toolsRentalResponseDTO.setDiscountPercent(discountPercent);
 
         //// TODO: 28/03/24 Round it to cents
